72e91a1043225e1e8e20573a6b5bca66cb82ef05
Jose Claudivan <claudivan0103@gmail.com>
2020-10-02 00:09:38 -0300

    Changing the sequence background color

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
-var player = null;
-var selectedPlayer = document.getElementById('jogador-selecionado')
+var player, winner = null;
+var selectedPlayer = document.getElementById('jogador-selecionado');
+var selectedWinner = document.getElementById('vencedor-selecionado')
 var squares = document.getElementsByClassName('quadrado');
 
 changePlayer('X');
 
 function selectSquare(id) {
-    var quadrado = document.getElementById(id);
-    if(quadrado.innerHTML !== '-') {
+    var square = document.getElementById(id);
+    if(square.innerHTML !== '-') {
         return;
     }
 
-    quadrado.innerHTML = player;
-    quadrado.style.color = '#000';
+    square.innerHTML = player;
+    square.style.color = '#000';
 
     if (player === 'X') {
         player = 'O';
@@ -20,6 +21,7 @@ function selectSquare(id) {
     }
     
     changePlayer(player);
+    checkWinner();
 }
 
 function changePlayer(valor) {
@@ -40,13 +42,63 @@ function checkWinner() {
 
     if (checkSequence(square1, square2, square3)) {
         changeSquareColor(square1, square2, square3);
+        changeWinner(square1);
+        return;
     }
+
+    if (checkSequence(square4, square5, square6)) {
+        changeSquareColor(square4, square5, square6);
+        changeWinner(square4);
+        return;
+    }
+
+    if (checkSequence(square7, square8, square7)) {
+        changeSquareColor(square7, square8, square7);
+        changeWinner(square7);
+        return;
+    }
+
+    if (checkSequence(square1, square4, square9)) {
+        changeSquareColor(square1, square4, square9);
+        changeWinner(square4);
+        return;
+    }
+
+    if (checkSequence(square2, square5, square8)) {
+        changeSquareColor(square2, square5, square8);
+        changeWinner(square2);
+        return;
+    }
+
+    if (checkSequence(square3, square6, square9)) {
+        changeSquareColor(square3, square6, square9);
+        changeWinner(square3);
+        return;
+    }
+
+    if (checkSequence(square1, square5, square9)) {
+        changeSquareColor(square1, square5, square9);
+        changeWinner(square1);
+        return;
+    }
+
+    if (checkSequence(square3, square5, square7)) {
+        changeSquareColor(square3, square5, square7);
+        changeWinner(square3);
+        return;
+    }
+
+}
+
+function changeWinner(square) {
+    winner = square.innerHTML;
+    selectedWinner.innerHTML = winner;
 }
 
 function changeSquareColor(square1, square2, square3) {
-    square1.style.color = '#0f8';
-    square2.style.color = '#0f8';
-    square3.style.color = '#0f8';
+    square1.style.background = '#0f8';
+    square2.style.background = '#0f8';
+    square3.style.background = '#0f8';
 }
 
 function checkSequence(square1, square2, square3) {
